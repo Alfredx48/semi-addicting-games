061404ca817258d6b26f1b80c8802d55c0c6500c
Cameron <Cameron@dansclan.com>
2022-11-17 13:00:12 -0800

    Styling

--- src/Reviews/Form.js
+++ src/Reviews/Form.js
@@ -1,101 +1,100 @@
 import React, { useState, useEffect } from "react";
 import Reviews from "./Reviews";
+import { motion } from "framer-motion";
 
 const Form = () => {
-  const [reviews, setReviews] = useState([])
+  const [reviews, setReviews] = useState([]);
 
-    useEffect(() => {
-      fetch("http://localhost:8000/posts")
-      .then(res => res.json())
-      .then(reviewData => {
-        setReviews(reviewData)})
-    },[])
-    console.log(reviews)
+  useEffect(() => {
+    fetch("http://localhost:8000/posts")
+      .then((res) => res.json())
+      .then((reviewData) => {
+        setReviews(reviewData);
+      });
+  }, []);
+  console.log(reviews);
 
-
-  const [formData, setFormData] = 
-  useState( {
-    id:"",
-    game:"",
+  const [formData, setFormData] = useState({
+    id: "",
+    game: "",
     name: "",
     rating: 0,
-    review:""
-  })
+    review: "",
+  });
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-   const configObj = { 
+    e.preventDefault();
+    const configObj = {
       method: "POST",
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify(formData)
-    }
+      body: JSON.stringify(formData),
+    };
     fetch("http://localhost:8000/posts", configObj)
-    .then(res => res.json())
-    .then(review => {
-      console.log(review)
-      setReviews(reviews => [...reviews, review])
-    })
-
-  }
+      .then((res) => res.json())
+      .then((review) => {
+        console.log(review);
+        setReviews((reviews) => [...reviews, review]);
+      });
+  };
 
   const handleChange = (e) => {
     setFormData((prevState) => {
       return {
         ...prevState,
         [e.target.name]: e.target.value,
-      }
-    })
-  }
+      };
+    });
+  };
 
   return (
     <div className="formReviews">
-      <form 
-      className="form"
-      onSubmit={handleSubmit}
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ rotate: 360, scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 150,
+          damping: 20,
+        }}
       >
-        <label>Game:</label>
-        <select
-        onChange={handleChange}
-        name="game"
-        >
-          <option>TicTacToe</option>
-          <option>HangMan</option>
-          <option>ConstructionZone</option>
-        </select>
-        <label>Name:</label>
-        <input
-        value={formData.name}
-        onChange={handleChange} 
-        placeholder="Name"
-        name="name"
-        >
-        </input>
-        <label>Rating:</label>
-        <select
-        onChange={handleChange}
-        name="rating"
-        >
-          <option>0</option>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
-        <label>Reviews:</label>
-        <textarea
-        onChange={handleChange} 
-        placeholder="Review"
-        name="review"
-        ></textarea>
         <br/>
-        <button>Submit</button>
-      </form>
-      <br/>
+        <form className="form" onSubmit={handleSubmit}>
+          <label>Game:</label>
+          <select onChange={handleChange} name="game">
+            <option>TicTacToe</option>
+            <option>HangMan</option>
+            <option>ConstructionZone</option>
+          </select>
+          <label>Name:</label>
+          <input
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Name"
+            name="name"
+          ></input>
+          <label>Rating:</label>
+          <select onChange={handleChange} name="rating">
+            <option>0</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+          <label>Reviews:</label>
+          <textarea
+            onChange={handleChange}
+            placeholder="Review"
+            name="review"
+          ></textarea>
+          <br />
+          <button>Submit</button>
+        </form>
+        <br />
+      </motion.div>
       <Reviews reviews={reviews} />
-
     </div>
   );
 };
--- src/Reviews/Reviews.js
+++ src/Reviews/Reviews.js
@@ -1,17 +1,41 @@
 import React from "react";
+import {motion} from "framer-motion"
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
 
 const Reviews = ({ reviews }) => {
   const mappedReviews = reviews.map((review) => {
     return (
-        <>
-
-      <div className="reviews" key={review.id} >
+      <>
+      <motion.div
+        variants={variants}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        className="reviews"
+      >
+      <div key={review.id} >
         <h1>Name:{review.name}</h1>
         <h3>Game:{review.game}</h3>
         <h3>Rating:{review.rating}</h3>
         <h4>Review:</h4>
         <p>{review.review}</p>
       </div>
+      </motion.div>
       <br/>
       </>
     );
